e1c38d4da909357d66b7bd13e0be490bdfcffa9f
Tomasz 'Trog' Welman <tomasz.w@yggdrasilgaming.com>
2017-12-17 13:00:22 +0100

    notification and background downloads

--- client.py
+++ client.py
@@ -4,17 +4,52 @@ import socket
 import ssl
 import re
 import sys
-import psycopg2
-from threading import Lock
+import os
+import subprocess
+from threading import Thread, Lock
 
 RTT_HOST = '0.0.0.0'
 RTT_PORT = 13014
-RTT_BACKLOG = 5
 RTT_RECV_SIZE = 4096
+RTT_ENCODING = 'utf8'
 RTT_CERT_FILE = './client.pem'
 RTT_CERT_KEY = './client.key'
 RTT_stderr_lock = Lock()
 RTT_stderr = sys.stderr
+RTT_chdir = '/home/user/'
+
+class Downloader(Thread):
+    def __init__(self, entries):
+        Thread.__init__(self)
+        self.entries = entries
+
+    def run(self):
+        for entry in self.entries:
+            cmd = ['xd.sh', "'" + entry['entry'] + "'"]
+            log(cmd)
+            try:
+                os.chdir(RTT_chdir)
+                subprocess.call(cmd)
+            except Exception as e:
+                log(e)
+
+
+def is_daemon():
+    return len(sys.argv) == 2 and sys.argv[1] == '-d'
+
+
+def is_notify_d():
+    return len(sys.argv) == 2 and sys.argv[1] == '-n'
+
+
+def get_limit():
+    limit = 10
+    if len(sys.argv) > 1:
+        try:
+            limit = int(sys.argv[1])
+        except:
+            limit = 10
+    return limit
 
 
 def log(log_item):
@@ -25,7 +60,7 @@ def log(log_item):
 
 def process_response(data):
     content_length = None
-    data = data.decode('utf8')
+    data = data.decode(RTT_ENCODING)
     # log(data)
     hdr_end_idx = data.find('\r\n\r\n')
     if hdr_end_idx >= 0:
@@ -36,25 +71,55 @@ def process_response(data):
     try:
         req = json.loads(data)
     except Exception as e:
-        # log(e)
+        log(e)
         return None
     keys = req.keys()
     if 'jsonrpc' in keys and req['jsonrpc'] == '2.0' and 'result' in keys and 'id' in keys:
         result = req['result']
         if 'torrents' in result.keys():
             for t in result['torrents']:
-                print(str(t['id']) + ' ' + t['torrent'])
+                log(str(t['id']) + ' ' + t['torrent'])
+        elif 'notify_d' in result.keys():
+            log(result['notify_d'])
+            Downloader(result['notify_d']).start()
     return content_length
 
 
+def method_list(ssl_socket, limit):
+    ssl_socket.sendall(('{"jsonrpc": "2.0", "method": "list", "params": {"limit": ' + str(limit) + '}, "id": 1}').encode(RTT_ENCODING))
+    data = ssl_socket.recv(RTT_RECV_SIZE)
+    total_data = bytearray()
+    content_len = None
+    while data is not None and len(data) > 0:
+        total_data += data
+        c_len = process_response(total_data)
+        if c_len is not None:
+            content_len = c_len
+        if content_len is not None and len(total_data) >= content_len:
+            break
+        data = ssl_socket.recv(RTT_RECV_SIZE)
+
+
+def method_subscribe(ssl_socket):
+    ssl_socket.sendall(('{"jsonrpc": "2.0", "method": "subscribe", "id": 1}').encode(RTT_ENCODING))
+    total_data = bytearray()
+    content_len = None
+    data = ssl_socket.recv(RTT_RECV_SIZE)
+    while data is not None and len(data) > 0:
+        total_data += data
+        c_len = process_response(total_data)
+        if c_len is not None:
+            content_len = c_len
+        if content_len is not None and len(total_data) >= content_len:
+            total_data = bytearray()
+        data = ssl_socket.recv(RTT_RECV_SIZE)
+
+
+def method_notify_d(ssl_socket):
+    ssl_socket.sendall(('{"jsonrpc": "2.0", "method": "notify_d", "id": 1}').encode(RTT_ENCODING))
+
+
 def run():
-    if len(sys.argv) > 1:
-        try:
-            limit = int(sys.argv[1])
-        except:
-            limit = 10
-    else:
-        limit = 10
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((RTT_HOST, RTT_PORT))
@@ -71,19 +136,12 @@ def run():
         log('conn refused: ' + e.strerror)
         s.close()
         return
-    client = None
-    ssl_socket.sendall(('{"jsonrpc": "2.0", "method": "list", "params": {"limit": ' + str(limit) + '}, "id": 1}').encode('utf8'))
-    data = ssl_socket.recv(RTT_RECV_SIZE)
-    total_data = bytearray()
-    content_len = None
-    while data is not None and len(data) > 0:
-        total_data += data
-        c_len = process_response(total_data)
-        if c_len is not None:
-            content_len = c_len
-        if content_len is not None and len(total_data) >= content_len:
-            break
-        data = ssl_socket.recv(RTT_RECV_SIZE)
+    if is_daemon():
+        method_subscribe(ssl_socket)
+    elif is_notify_d():
+        method_notify_d(ssl_socket)
+    else:
+        method_list(ssl_socket, get_limit())
 
 
 run()
--- server.py
+++ server.py
@@ -5,10 +5,13 @@ import ssl
 import re
 import sys
 import psycopg2
+import select
+import time
 from threading import Lock
 
 RTT_HOST = '0.0.0.0'
 RTT_PORT = 13014
+RTT_ENCODING = 'utf8'
 RTT_BACKLOG = 5
 RTT_RECV_SIZE = 512
 RTT_CERT_FILE = './server.pem'
@@ -17,11 +20,13 @@ RTT_CA_FILE = './ca.pem'
 RTT_DB_NAME = ''
 RTT_DB_USER = ''
 RTT_DB_PASS = ''
+RTT_WRITE_ALLOWED_EMAILS = ['x@x.xe']
 RTT_RE_VENDOR = re.compile('-[a-z0-9]+\.+torrent$', re.I)
 RTT_RE_VEN_END = re.compile('\.+torrent$', re.I)
 RTT_RE_EPISODE = re.compile('(\.S[0-9]{2}E[0-9]{2}\.)|(\.S[0-9]{2}E[0-9]{2}-E?[0-9]{2}\.)', re.I)
 RTT_stderr_lock = Lock()
 RTT_stderr = sys.stderr
+RTT_notify_d = None
 
 
 def log(log_item):
@@ -66,6 +71,20 @@ def get_torrents(n_limit):
     return torrents
 
 
+def get_from_queue(from_timestamp):
+    conn = psycopg2.connect('dbname=' + RTT_DB_NAME + ' user=' + RTT_DB_USER + ' password=' + RTT_DB_PASS)
+    c = conn.cursor()
+    c.execute('SELECT torrent,downloaded_at FROM downloaded_queue ORDER BY id')
+    torrents = []
+    for t in c.fetchall()[::-1]:
+        d = get_details(t[0])
+        dt = int(time.mktime(t[1].timetuple()))
+        torrents.append({'entry': t[0], 'downloaded_at': dt, 'vendor': d[0], 'season': d[1], 'episodes': d[2]})
+    c.close()
+    conn.close()
+    return torrents
+
+
 def get_json_response(result, request_id):
     json_obj = {}
     json_obj['jsonrpc'] = '2.0'
@@ -79,26 +98,34 @@ def get_json_response_list(n_limit, request_id):
     return get_json_response({'torrents': torrents}, request_id)
 
 
+def get_json_response_notify_d(torrents, request_id):
+    return get_json_response({'notify_d': torrents}, request_id)
+
+
+def get_json_response_subscribe(request_id):
+    return get_json_response({'subscribed': True}, request_id)
+
+
 def prepare_response(response_json):
     response = json.dumps(response_json)
-    encodedResponse = response.encode('utf8')
+    encodedResponse = response.encode(RTT_ENCODING)
     respLen = len(encodedResponse)
     fullContent = 'Content-Length: ' + str(respLen) + "\r\n\r\n" + response
     return fullContent
 
 
-def process_request(data):
+def process_request(data, write_auth):
     fullContent = None
-    data = data.decode('utf8')
+    data = data.decode(RTT_ENCODING)
     # log(data)
     try:
         req = json.loads(data)
     except:
         return None
     keys = req.keys()
-    if 'jsonrpc' in keys and req['jsonrpc'] == '2.0' and 'method' in keys and 'id' in keys and 'params' in keys:
+    if 'jsonrpc' in keys and req['jsonrpc'] == '2.0' and 'method' in keys and 'id' in keys:
         method = req['method']
-        if 'list' == method:
+        if 'list' == method  and 'params' in keys:
             if 'limit' in req['params'].keys():
                 try:
                     limit = int(req['params']['limit'])
@@ -108,37 +135,123 @@ def process_request(data):
                 limit = 10
             response_json = get_json_response_list(limit, req['id'])
             fullContent = prepare_response(response_json)
-        elif 'x' == method:
-            pass
+        elif write_auth:
+            if 'notify_d' == method:
+                log('notify_d')
+                global RTT_notify_d
+                fullContent = ''
+                response_json = get_json_response_notify_d(get_from_queue(None), req['id'])
+                RTT_notify_d = prepare_response(response_json)
+            elif 'subscribe' == method:
+                log('client subscribed')
+                response_json = get_json_response_subscribe(req['id'])
+                fullContent = prepare_response(response_json)
     return fullContent
 
 
-def run():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((RTT_HOST, RTT_PORT))
-    s.listen(RTT_BACKLOG)
-    while True:
-        client, address = s.accept()
-        log('accepted: ' + str(address))
-        try:
-            ssl_client = ssl.wrap_socket(client, server_side=True, certfile=RTT_CERT_FILE, keyfile=RTT_CERT_KEY, ssl_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_REQUIRED, ca_certs=RTT_CA_FILE)
-        except ssl.SSLError as e:
-            log('no client cert: ' + e.strerror)
-            client.close()
-            continue
-        except ConnectionResetError as e:
-            log('conn reset: ' + e.strerror)
-            client.close()
-            continue
-        client = None
+def get_ssl_client(client):
+    try:
+        ssl_client = ssl.wrap_socket(client, server_side=True, certfile=RTT_CERT_FILE, keyfile=RTT_CERT_KEY, ssl_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_REQUIRED, ca_certs=RTT_CA_FILE)
+    except ssl.SSLError as e:
+        log('no client cert: ' + e.strerror)
+        client.close()
+        return None
+    except ConnectionResetError as e:
+        log('conn reset: ' + e.strerror)
+        client.close()
+        return None
+    return ssl_client
+
+
+def handle_client(ssl_client):
+    write_auth = is_write_authorized(ssl_client)
+    total_data = bytes()
+    log('handle_client')
+    #ssl_client.settimeout(1)
+    try:
         data = ssl_client.recv(RTT_RECV_SIZE)
-        while data is not None and len(data) > 0:
-            response = process_request(data)
-            if response is not None:
-                # log(response)
-                ssl_client.sendall(response.encode('utf8'))
+    except:
+        log('hc timeout1')
+        return True
+    data_len = len(data)
+    while data is not None and data_len > 0:
+        total_data += data
+        response = process_request(total_data, write_auth)
+        if response is not None:
+            # log(response)
+            ssl_client.sendall(response.encode(RTT_ENCODING))
+            log('client handled with response')
+            break
+        try:
             data = ssl_client.recv(RTT_RECV_SIZE)
+        except:
+            log('hc timeout2')
+        data_len = len(data)
+    log('client end')
+    return data_len < 1
+
+
+def is_write_authorized(ssl_client):
+    cert = ssl_client.getpeercert()
+    log(cert)
+    if cert is not None and 'subject' in cert.keys():
+        for pair in cert['subject']:
+            if len(pair) > 0:
+                key, value = pair[0]
+                if 'emailAddress' == key:
+                    return value in RTT_WRITE_ALLOWED_EMAILS
+    return False
+
+
+def clean_clients(old_clients):
+    clients = []
+    for c in old_clients:
+        if c.fileno() > 2:
+            clients.append(c)
+        else:
+            log('removing client: ' + str(c))
+    return clients
+
+
+def run():
+    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_server.bind((RTT_HOST, RTT_PORT))
+    sock_server.listen(RTT_BACKLOG)
+    clients = []
+    while True:
+        log('select loop')
+        clients = clean_clients(clients)
+        fds_read, fds_write, fds_error = select.select([sock_server] + clients, [sock_server], [sock_server] + clients)
+        log(fds_read)
+        log(fds_write)
+        log(fds_error)
+        if sock_server in fds_read:
+            log('sock_server in fds_read')
+            client, address = sock_server.accept()
+            log('accepted: ' + str(address))
+            clients.append(get_ssl_client(client))
+        elif sock_server in fds_write:
+            log('sock_server in fds_write')
+        elif sock_server in fds_error:
+            log('sock_server in fds_error')
+            break
+        for fd_read in fds_read:
+            if fd_read in clients:
+                if handle_client(fd_read):
+                    log('removing due to close: ' + str(fd_read))
+                    clients.remove(fd_read)
+                global RTT_notify_d
+                if RTT_notify_d is not None:
+                    for c in clients:
+                        log('sendin n to: ' +str(c))
+                        c.sendall(RTT_notify_d.encode(RTT_ENCODING))
+                    RTT_notify_d = None
+        for fd_error in fds_error:
+            if fd_error in clients:
+                log('removing due to error: ' + str(fd_error))
+                clients.remove(fd_error)
+                fd_error.close()
 
 
 run()
