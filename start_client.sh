9780c8c2adf17d17d1af4257d54e8497be67aa96
Tomasz Welman <welmantw@amazon.com>
2019-11-09 08:17:30 +0000

    missing env vars definition in start_client.sh

--- start_client.sh
+++ start_client.sh
@@ -6,4 +6,6 @@ RTT_CERT_FILE='./client.pem' \
 RTT_CERT_KEY='./client.key' \
 RTT_CA_FILE='./ca.pem' \
 RTT_CHDIR='/home/user/' \
+RTT_LATEST_FILE='./latest.txt' \
+RTT_PID_PATH='./pid' \
 python3 ./client.py $@
