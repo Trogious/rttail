98860bbdca864160c6a338e668e6a1ee74e9910e
Trogious <trog@swmud.net>
2020-02-20 12:40:35 +0000

    minor config

--- start_client.sh
+++ start_client.sh
@@ -9,7 +9,9 @@ RTT_CERT_KEY='./client.key' \
 RTT_CA_FILE='./ca.pem' \
 RTT_CHDIR='.' \
 RTT_LATEST_FILE='./latest.txt' \
-RTT_PID_PATH='./pid'
+RTT_PID_PATH='./pid' \
+RTT_SOCKET_TIMEOUT=60 \
+RTT_DOWNLOAD_CMD='txd.sh'
 if [ $# -eq 1 -a x"$1" = x"-n" ]; then
   $PYTHON $CLIENT $@ &
 else
